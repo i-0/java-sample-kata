8b11062eab462b883d0ff1d9e24e55fcd52724f8
Christoph Gierling <christoph.gierling@rewe-digital.com>
2024-08-22 23:31:22 +0200

    Setup sample unit test

---
+++ src/test/java/org/i0/tryouts/MainTest.java
@@ -0,0 +1,12 @@
+package org.i0.tryouts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class MainTest {
+    @Test
+    void giveMeAGoodName() {
+        fail();
+    }
+}
